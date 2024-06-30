50ea3726c007ff1dcde577bbb95ac04e086440e2
maetsuyo <maenoorehatuyokatta8009@gmail.com>
2024-06-30 22:11:38 +0900

    白昼夢.pyの解き方を変えた。

--- 白昼夢.py
+++ 白昼夢.py
@@ -1,29 +1,10 @@
-bool = False
-answer = "NO"
 S = input()
-T = ""
-words = ["dream", "dreamer", "erase", "eraser"]
-for i in words:
-    T += i
-    if T in S:
-        if S == T:
-            answer = "YES"
-            bool = True
-            break
-        else:
-            for j in words:
-                temp = T
-                T += j
-                if S == T:
-                    answer = "YES"
-                    bool = True
-                    break
-                else:
-                    T = temp
-            T = ""
-            if bool == True:
-                break 
-    else:
-        T = ""
+words = ["eraser", "erase", "dreamer", "dream"]
 
-print(answer)
+for i in words:
+    S = S.replace(i, "")
+
+if S == "":
+    print("YES")
+else:
+    print("NO")
