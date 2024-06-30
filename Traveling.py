30f1323dbc3b88e79fcbbb62a717be1d42063702
maetsuyo <maenoorehatuyokatta8009@gmail.com>
2024-06-30 21:28:14 +0900

    最終的な時刻tの時にぴったり点(x,y)にいるという処理がわからない。

--- Traveling.py
+++ Traveling.py
@@ -1,10 +1,10 @@
 count = 0
-answer = "NO"
-bool = False
+answer = "No"
 t = []
 x = []
 y = []
 coord = [0,0]
+coord[1] = coord[1]
 
 N = int(input())
 for i in range(N):
@@ -14,19 +14,28 @@ for i in range(N):
     y.append(temp_y)
 
 for j in range(N):
-    for k in range(0, x[j]):
-        coord[0] += 1
-        count += 1
-        if t[0] < count:
-            bool = True
-            break
-    if bool == True:
-        exit
-    for l in range(0, y[j]):
-        coord += [1]
-        count += 1
-        if t[0] < count:
-            bool = True
-            break
-    if bool == True:
-        exit
+    if coord[0] < x[j]:
+        for k in range(coord[0], x[j]):
+            coord[0] += 1
+            count += 1
+    if coord[0] > x[j]:
+        for k in range(coord[0], x[j]):
+            coord[0] -= 1
+            count += 1
+    if t[j] < count:
+        break
+    if coord[1] < y[j]:
+        for l in range(coord[1], y[j]):
+            coord[1] += 1
+            count += 1
+    if coord[1] > x[j]:
+        for l in range(coord[1], y[j]):
+            coord[1] -= 1
+            count += 1
+    if t[j] < count:
+        break
+
+    if j == N-1:
+        answer = "Yes"
+
+print(answer)
