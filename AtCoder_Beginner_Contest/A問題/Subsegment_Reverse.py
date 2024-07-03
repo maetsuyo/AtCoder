7d9b435702e06f398c4e2513b30d673d830de588
maetsuyo <maenoorehatuyokatta8009@gmail.com>
2024-07-03 12:04:37 +0900

    12:04

---
+++ AtCoder_Beginner_Contest/A問題/Subsegment_Reverse.py
@@ -0,0 +1,13 @@
+import math
+
+N,L,R = map(int, input().split())
+num_list = []
+for i in range(N):
+    num_list.append(i+1)
+
+reverse_list = []
+for j in range(L, R+1):
+    reverse_list.append(j)
+
+print(num_list)
+print(reverse_list)
